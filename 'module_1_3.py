723622381dd71cdcd8866631108d3d8f21284964
KimIgor2024 <kim317267@gmail.com>
2024-10-27 14:24:26 +0300

    Add files via upload

---
+++ 'module_1_3.py
@@ -0,0 +1,25 @@
+name = 5
+print(name ,type(name))
+name = '5'
+print(name , type(name))
+name = [5]
+print(name , type(name))
+name = 5.0
+print(name , type(name))
+name = "Igor"
+print(name , type(name))
+
+age = 53
+print(int(age))
+new_age = age + 15
+print(int(age + new_age))
+new__age = age + 17
+print(int(age + new__age))
+is_student ='true'
+print(is_student)
+print(age + new_age <= age + new__age)
+
+
+
+
+
