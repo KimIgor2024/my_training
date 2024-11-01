66e2612294b9af96d4f0b1e0243f05a85e258b63
KimIgor2024 <kim317267@gmail.com>
2024-11-02 01:02:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,59 @@
-# This is a sample Python script.
+"Дополнительное практическое задание по моду"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#umbers = [1, 2, 3, 4, 5]
+#total = sum(numbers)
+#print(total)  # Вывод: 15
 
+data = ['Johnny','Bilbo','Steve','Khendrik','Aaron']     #['Aaron', 'Bilbo', 'Johnny', 'Khendrik', 'Steve']
+print(sorted(data ))
+print(sorted)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+numbers_1 = [5 ,3 ,3 ,5 ,4]
+total_1 = sum(numbers_1)
+print(total_1)                       #    Aaron :( 20 / 5 ) = 4.0
+my_list_1 = [5 ,3 ,3 ,5 ,4]
+numbers_1 = len(my_list_1)
+print(numbers_1)
 
+numbers_2 = [2, 2, 2, 3]
+total_2 = sum(numbers_2)
+print(total_2)                       #    Bilbo : ( 9 / 4 ) = 2.25
+my_list_2 = [2 ,2 ,2 ,3]
+numbers_2 = len(my_list_2)
+print(numbers_2)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+numbers_3 = [4 ,5 ,5 ,2]
+total_3 = sum(numbers_3)
+print(total_3)                       #    Johnny :  ( 16 / 4 ) = 4.0
+my_list_3 = [4 ,5 ,5 ,2]
+numbers_3 = len(my_list_3)
+print(numbers_3)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print('Hello,world')
+numbers_4 = [4 ,4 ,3]
+total_4 = (numbers_4)
+print(total_4)                       #     Khendrik : ( 11 /3 ) = 3.6666666666666665
+my_list_4 = [4 ,4 ,3]
+numbers_4 = len(my_list_4)
+print(numbers_4)
+
+numbers_5 = [5 ,5 ,5 ,4 ,5]
+total = sum(numbers_5)
+print(total)                       #     Steve :(  24 / 5 ) = 4.8
+my_list_5 = [5 ,5 ,5 ,4 ,5]
+numbers_5 = len(my_list_5)
+print(numbers_5)
+
+Aaron = (20/5 )
+print(Aaron)
+Bilbo =( 9 / 4 )
+print(Bilbo)
+Johnny = ( 16 / 4 )
+print(Johnny)
+Khendrik = ( 11 / 3 )
+print(Khendrik)
+Steve = ( 24 / 5 )
+print(Steve)
+
+my_dict = {'Aaron': 4.0 ,'Bilbo': 2.25 ,'Johnny': 4.0 ,'Khendrik': 3.6666666666666665 ,'Sneve': 4.8 }
+print(my_dict)
+#
